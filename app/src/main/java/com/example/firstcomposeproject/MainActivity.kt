21417442f4ecf039162931733b11d7406f16fc05
avlnkov <alex.lenkov@mail.ru>
2025-03-15 21:49:27 +0300

    added card view

--- app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
@@ -4,23 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.example.firstcomposeproject.ui.theme.InstagramHeadContainer
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,47 +12,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         setContent {
-            TimesTable()
+            InstagramHeadContainer()
         }
     }
 }
 
-@Preview
-@Composable
-fun TimesTable() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        repeat(9) { i ->
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f)
-            ) {
-                repeat(9) { j ->
-
-                    val color: Color = if ((i % 2 == 0 && j % 2 == 0) ||
-                        (i % 2 != 0 && j % 2 != 0)
-                    ) {
-                        Color.Blue
-                    } else {
-                        Color.Yellow
-                    }
-
-                    Box(
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .weight(1f)
-                            .border(width = 1.dp, Color.DarkGray)
-                            .background(color),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(text = "${(i + 1) * (j + 1)}")
-                    }
-                }
-            }
-        }
-
-    }
-}
