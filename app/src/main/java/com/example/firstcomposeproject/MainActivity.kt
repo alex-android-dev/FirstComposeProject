ce03ddea5b963022cab19a4446b2267418e2cc31
avlnkov <alex.lenkov@mail.ru>
2025-03-15 23:03:15 +0300

    added card

--- app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
@@ -4,6 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.firstcomposeproject.ui.theme.InstagramHeadContainer
 
 class MainActivity : ComponentActivity() {
@@ -17,3 +28,23 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+@Preview
+@Composable
+fun CardTest() {
+    Card(
+        shape = RoundedCornerShape(
+            topStart = 4.dp,
+            topEnd = 4.dp
+        ),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.Green,
+            contentColor = Color.White
+        ),
+        border = BorderStroke(1.dp, Color.Red)
+    ) {
+        Text(
+            modifier = Modifier.padding(8.dp),
+            text = "hello gays")
+    }
+}
