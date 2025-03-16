b832334251c663c27e784968e6268272250371a3
avlnkov <alex.lenkov@mail.ru>
2025-03-16 17:28:37 +0300

    added white background for IG logo

--- app/src/main/java/com/example/firstcomposeproject/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/com/example/firstcomposeproject/ui/theme/InstagramProfileCard.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -86,10 +87,10 @@ fun InstagramHead(posts: String, followers: String, following: String) {
 private fun InstagramLogo() {
     Image(
         modifier = Modifier
-            .size(50.dp)
-            .padding(5.dp)
-            .clip(RoundedCornerShape(5.dp))
-            .background(Color.White),
+            .size(35.dp)
+            .clip(CircleShape)
+            .background(Color.White)
+            .padding(5.dp),
         painter = painterResource(R.drawable.instagram_1_svgrepo_com),
         contentDescription = "App icon",
     )
