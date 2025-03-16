01d5eafd4feb31debcfba99bebde82e6039880bd
avlnkov <alex.lenkov@mail.ru>
2025-03-16 16:53:09 +0300

    added image

--- app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
@@ -4,22 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.firstcomposeproject.ui.theme.FirstComposeProjectTheme
 import com.example.firstcomposeproject.ui.theme.InstagramHeadContainer
 
@@ -34,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier
                         .fillMaxSize()
                         .background(MaterialTheme.colorScheme.background)
-                // Чтобы весь экран настраивал тот цвет, который соответствует теме
+                    // Чтобы весь экран настраивал тот цвет, который соответствует теме
                 ) {
 
                 }
@@ -42,4 +38,17 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun TestImage() {
+    Image(
+        modifier = Modifier
+            .fillMaxSize() // Заполнить все пространство
+            .clip(CircleShape), // Обрезать по форме (В данном случае это шейп)
+        painter = painterResource(R.drawable.ic_launcher_background),
+        contentDescription = "",
+        contentScale = ContentScale.FillHeight // Растягивается по высоте или горизонту или во все стороны
+    )
 }
--- app/src/main/java/com/example/firstcomposeproject/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/com/example/firstcomposeproject/ui/theme/InstagramProfileCard.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -18,7 +20,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -80,16 +84,12 @@ fun InstagramHead(posts: String, followers: String, following: String) {
 
 @Composable
 private fun InstagramLogo() {
-    Column(
-        modifier = Modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            modifier = Modifier.size(50.dp),
-            painter = painterResource(R.drawable.instagram_1_svgrepo_com),
-            contentDescription = "App icon"
-        )
-    }
+    Image(
+        modifier = Modifier
+            .size(50.dp),
+        painter = painterResource(R.drawable.instagram_1_svgrepo_com),
+        contentDescription = "App icon",
+    )
 }
 
 @Composable
