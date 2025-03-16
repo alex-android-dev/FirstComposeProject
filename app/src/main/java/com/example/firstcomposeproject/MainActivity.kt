73cf1a4dc5d5e04aa646f5824b5cc465d499706e
avlnkov <alex.lenkov@mail.ru>
2025-03-16 17:16:50 +0300

    test modifier

--- app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
@@ -4,11 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.firstcomposeproject.ui.theme.FirstComposeProjectTheme
 import com.example.firstcomposeproject.ui.theme.InstagramHeadContainer
 
@@ -31,4 +43,27 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun TestImage() {
+    Box(
+        modifier = Modifier
+            .size(200.dp)
+            .background(Color.Yellow)
+    ) {
+        Image(
+            modifier = Modifier
+                .background(Color.Green)
+                .padding(25.dp)
+                .size(100.dp)
+                .background(Color.Red)
+                .padding(25.dp),
+            painter = ColorPainter(Color.Gray),
+            contentDescription = "",
+            contentScale = ContentScale.FillHeight,
+        )
+    }
+
 }
